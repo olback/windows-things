195c790a6c3be879f776ba166a1014ace38face6
Edwin <git@olback.net>
2017-04-06 17:31:23 +0200

    Update example.bat

--- example.bat
+++ example.bat
@@ -1,1 +1,24 @@
+@ECHO OFF
+chcp 65001
 
+REM Created by github.com/olback
+
+REM ----- Groups -----
+REM Admin (Swedish OS): administratörer
+REM Admin (English OS): administrators
+
+
+REM ----- Set the user, domain and localgroup here -----
+SET user=Alex
+SET domain=SomeWindowsDomain
+SET group=administrators
+
+
+
+REM ----- Do not edit below this line -----
+
+net localgroup %group% %domain%\%user% /add
+
+echo %domain%\%user% Is now added to the group %group%
+
+timeout -1
