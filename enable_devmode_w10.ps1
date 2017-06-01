5d847e777dca09a5f2d1015087efca65b13c2bf9
Edwin <git@olback.net>
2017-06-01 20:03:15 +0200

    Update enable_devmode_w10.ps1

--- enable_devmode_w10.ps1
+++ enable_devmode_w10.ps1
@@ -1,3 +1,3 @@
-# Add registry value to enable Developer
+# Add registry value to enable Developer Mode
 $RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
 New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
